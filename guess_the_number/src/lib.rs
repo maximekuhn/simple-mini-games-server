919bcd76c1403707d8bf1ae00629635b40999d26
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2023-08-08 22:13:24 +0200

    ADD: very basic implementation of guess the game + interactions with server

--- guess_the_number/src/lib.rs
+++ guess_the_number/src/lib.rs
@@ -1,14 +1,29 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use rand::{thread_rng, Rng};
+
+#[derive(Clone)]
+pub struct GuessTheNumber {
+    max_tries: u8,
+    tries_count: u8,
+    number_to_guess: i32,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl GuessTheNumber {
+    pub fn new(max_tries: u8, min_number: i32, max_number: i32) -> Self {
+        let tries_count = 0;
+        let number_to_guess = thread_rng().gen_range(min_number..=max_number);
+        Self {
+            max_tries,
+            tries_count,
+            number_to_guess,
+        }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn is_finished(&self) -> bool {
+        self.tries_count == self.max_tries
+    }
+
+    pub fn guess(&mut self, user_guess: i32) -> bool {
+        self.tries_count += 1;
+        self.number_to_guess.eq(&user_guess)
     }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,41 @@
-use axum::{routing::get, Router};
+use axum::{
+    extract::{FromRef, Path, State},
+    routing::{get, post},
+    Json, Router,
+};
+use guess_the_number::GuessTheNumber;
+use serde::{Deserialize, Serialize};
 use std::{
     error::Error,
     net::{Ipv4Addr, SocketAddr, SocketAddrV4},
 };
 
+#[derive(Serialize, Deserialize)]
+struct GameResponse {
+    hint: String,
+}
+
+async fn guess_the_number(
+    Path(user_guess): Path<i32>,
+    State(mut game): State<GuessTheNumber>,
+) -> Json<GameResponse> {
+    let hint = {
+        if game.guess(user_guess) {
+            String::from("WON")
+        } else {
+            String::from("TRY AGAIN")
+        }
+    };
+    GameResponse { hint }.into()
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let app = Router::new().route("/", get(|| async { "Hello, world" }));
+    let game = GuessTheNumber::new(10, 1, 20);
+
+    let app = Router::new()
+        .route("/games/guessthenumber/:user_guess", post(guess_the_number))
+        .with_state(game);
 
     axum::Server::bind(&SocketAddr::V4(SocketAddrV4::new(
         Ipv4Addr::new(127, 0, 0, 1),
