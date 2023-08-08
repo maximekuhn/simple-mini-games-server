33b1aba091f0693ee866a7fe7deb060494108d1d
Maxime Kuhn <pingouin2032.maxime@gmail.com>
2023-08-08 21:50:00 +0200

    ADD: guess the number game lib + workspace

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,19 @@
 use axum::{routing::get, Router};
+use std::{
+    error::Error,
+    net::{Ipv4Addr, SocketAddr, SocketAddrV4},
+};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error>> {
     let app = Router::new().route("/", get(|| async { "Hello, world" }));
 
-    axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    axum::Server::bind(&SocketAddr::V4(SocketAddrV4::new(
+        Ipv4Addr::new(127, 0, 0, 1),
+        3000,
+    )))
+    .serve(app.into_make_service())
+    .await?;
+
+    Ok(())
 }
